64c86b79cb455537bf20501b0cd0c614ed5d3ff3
Aditya Singh <adityasht@gmail.com>
2024-07-16 15:25:38 -0400

    Hours, Sec, min

--- SCOMP_Project_sum24/TIMER2.vhd
+++ SCOMP_Project_sum24/TIMER2.vhd
@@ -15,8 +15,7 @@ use IEEE.NUMERIC_STD.ALL;
 USE LPM.LPM_COMPONENTS.ALL;
 
 --Reset: 000 
---Basic Countdown: 001
---Basic Stopwatch: 010
+--Basic Stopwatch Start: 001
 --HEX_Out, LED_Out, INT Countdown1: 011
 --HEX_Out, LED_Out,  PAUSE Stopwatch1: 100
 --Full Countdown2: 101
@@ -35,16 +34,20 @@ ENTITY TIMER2 IS
 		  Mode5,
 		  Mode6,
         Mode7			:		IN    STD_LOGIC;
-        IO_DATA		:		INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-		  TIME_Out		:		OUT STD_LOGIC_VECTOR(23 DOWNTO 0);
-		  HexDisp_Out	:		OUT STD_LOGIC_VECTOR(23 DOWNTO 0)
+        IO_DATA		:		INOUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+		  
     );
 END TIMER2;
 
 ARCHITECTURE a OF TIMER2 IS
-    SIGNAL COUNT     : STD_LOGIC_VECTOR(15 DOWNTO 0);
-    SIGNAL IO_COUNT  : STD_LOGIC_VECTOR(15 DOWNTO 0); -- a stable copy of the count for the IO
-    SIGNAL OUT_EN    : STD_LOGIC;
+	 SIGNAL COUNT			: STD_LOGIC_VECTOR(23 DOWNTO 0);
+	 SIGNAL COUNTDOWN		: STD_LOGIC_VECTOR(23 DOWNTO 0);
+	 
+    SIGNAL IO_COUNT  	: STD_LOGIC_VECTOR(15 DOWNTO 0); -- a stable copy of the count for the IO
+    SIGNAL OUT_EN    	: STD_LOGIC;
+	 
+	 SIGNAL HR_M_Dec		: STD_LOGIC_VECTOR(15 DOWNTO 0);
+	 SIGNAL S_Cs_Dec		: STD_LOGIC_VECTOR(15 DOWNTO 0);
 
     BEGIN
 
@@ -60,15 +63,35 @@ ARCHITECTURE a OF TIMER2 IS
     );
 
     -- IO data should be driven when SCOMP is requesting data
-    OUT_EN <= (Mode1 AND NOT(IO_WRITE));
+	 OUT_EN <= (NOT(IO_WRITE));
 
-    PROCESS (CLOCK, RESETN, Mode2, IO_WRITE)
+    PROCESS (CLOCK, RESETN, IO_WRITE)
     BEGIN
-        IF (RESETN = '0' OR (Mode2 AND IO_WRITE) = '1') THEN
-            COUNT <= x"0000";
+        IF (RESETN = '0' OR (Mode0) = '1') THEN
+            COUNT <= x"000000";
+				COUNTDOWN <= x"FFFFFF";
+				
         ELSIF (rising_edge(CLOCK)) THEN
             COUNT <= COUNT + 1;
+				COUNTDOWN <= COUNTDOWN - 1;
         END IF;
+    END PROCESS;
+	 
+	 PROCESS (Mode2, COUNT, IO_COUNT, HR_M_Dec, S_Cs_Dec)
+			variable TEMP_IO_COUNT2, TEMP_IO_COUNT3 : std_logic_vector(15 DOWNTO 0);
+    BEGIN
+	 
+			TEMP_IO_COUnt2 := S_Cs_Dec;
+			TEMP_IO_COUnt3 := HR_M_Dec;
+			
+			IF Mode3 = '1' THEN
+				 IO_COUNT <= TEMP_IO_COUnt3;
+				 TEMP_IO_COUNT3 := TEMP_IO_COUNT3;
+			ELSIF Mode2 = '1' THEN
+				 IO_COUNT <= TEMP_IO_COUNT2;
+				 TEMP_IO_COUNT2 := TEMP_IO_COUNT2;
+			END IF;
+			
     END PROCESS;
 
     -- Use a latch to prevent IO_COUNT from changing while an IO operation is occuring.
@@ -85,22 +108,36 @@ ARCHITECTURE a OF TIMER2 IS
     END PROCESS;
 	 
 	 -- Converts Numbers into Decimal form for input to the HEx Display
-	 PROCESS (CLOCK, RESETN, COUNT, HexDisp_Out)
-		  variable temp : integer;
-        variable thousands, hundreds, tens, ones : integer;
+	 PROCESS (CLOCK, COUNT)
+    variable TEMP: INTEGER;
+    variable H_TENS, H_ONES, M_TENS, M_ONES, S_TENS, S_ONES, Cs_TENS, Cs_ONES: INTEGER;
 	 BEGIN
-	     temp 			:= IEEE.NUMERIC_STD.to_integer(IEEE.NUMERIC_STD.unsigned(COUNT));
-		  thousands		:= temp / 1000;
-        temp			:= temp mod 1000;
-        hundreds		:= temp / 100;
-        temp			:= temp mod 100;
-        tens			:= temp / 10;
-        ones			:= temp mod 10;
+  
+        TEMP := IEEE.NUMERIC_STD.to_integer(IEEE.NUMERIC_STD.unsigned(COUNT));
         
-		  HexDisp_Out(15 downto 12) <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(thousands, 4));
-        HexDisp_Out(11 downto 8) <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(hundreds, 4));
-        HexDisp_Out(7 downto 4) <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(tens, 4));
-        HexDisp_Out(3 downto 0) <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(ones, 4));
-    END PROCESS;
+        H_TENS := TEMP / 360000; 
+        H_ONES := (TEMP / 36000) mod 10;    
+        TEMP := TEMP mod 36000;
+        
+        M_TENS := TEMP / 6000;          -- Calculate tens of minutes
+        M_ONES := (TEMP mod 6000) / 600; -- Calculate ones of minutes
+        TEMP := TEMP mod 600;
+        
+        S_TENS := TEMP / 100;           -- Calculate tens of seconds
+        S_ONES := (TEMP mod 100) / 10;  -- Calculate ones of seconds
+        
+        Cs_TENS := (TEMP mod 10);       -- Calculate tens of centiseconds
+        Cs_ONES := TEMP mod 10;         -- Calculate ones of centiseconds
+        
+        HR_M_Dec(15 downto 12) <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(H_TENS, 4));
+        HR_M_Dec(11 downto 8)  <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(H_ONES, 4));
+        HR_M_Dec(7 downto 4)   <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(M_TENS, 4));
+        HR_M_Dec(3 downto 0)   <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(M_ONES, 4));
+        S_Cs_Dec(15 downto 12) <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(S_TENS, 4));
+        S_Cs_Dec(11 downto 8)  <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(S_ONES, 4));
+        S_Cs_Dec(7 downto 4)   <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(Cs_TENS, 4));
+        S_Cs_Dec(3 downto 0)   <= std_logic_vector(IEEE.NUMERIC_STD.to_unsigned(Cs_ONES, 4));
+
+	 END PROCESS;
 
 END a;
